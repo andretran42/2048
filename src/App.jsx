843d046c21d0e79c35e6abe263340010f0eed894
Andre Tran <atran03@wm.edu>
2023-09-21 23:03:59 -0400

    Tile styling

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,17 @@ import Tile from "../src/components/Tile/Tile";
 function App() {
   return (
     <>
-      <Tile value={4}></Tile>
-      <Tile value={2}></Tile>
+      <Tile value={2} x={0} y={0}></Tile>
+      <Tile value={4} x={100} y={0}></Tile>
+      <Tile value={8} x={200} y={0}></Tile>
+      <Tile value={16} x={300} y={0}></Tile>
+      <Tile value={32} x={300} y={100}></Tile>
+      <Tile value={64} x={200} y={100}></Tile>
+      <Tile value={128} x={100} y={100}></Tile>
+      <Tile value={256} x={0} y={100}></Tile>
+      <Tile value={512} x={0} y={200}></Tile>
+      <Tile value={1028} x={100} y={200}></Tile>
+      <Tile value={2048} x={200} y={200}></Tile>
     </>
   );
 }
--- src/components/Tile/Tile.jsx
+++ src/components/Tile/Tile.jsx
@@ -2,9 +2,12 @@ import React from "react";
 
 import "./Tile.css";
 
-function Tile({ value }) {
+function Tile({ value, x, y }) {
   return (
-    <div className={`tile tile-${value}`}>
+    <div
+      className={`tile tile-${value}`}
+      style={{ transform: `translate(${x}px, ${y}px)` }}
+    >
       <div className={`tile-inner`}>{value}</div>
     </div>
   );
